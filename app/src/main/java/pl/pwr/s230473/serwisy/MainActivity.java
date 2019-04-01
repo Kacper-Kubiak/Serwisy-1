098ae799d492b354699fc58fb8c22dd5600e7149
kacper <mYdTka2sk5>
2019-04-01 23:35:09 +0200

    RAW notification test.

--- app/src/main/java/pl/pwr/s230473/serwisy/MainActivity.java
+++ app/src/main/java/pl/pwr/s230473/serwisy/MainActivity.java
@@ -1,11 +1,22 @@
 package pl.pwr.s230473.serwisy;
 
+import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
